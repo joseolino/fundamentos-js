1bc89b9435d9f9b228baf0d3ca22baa6003092da
JOSÉ OLINO DE CAMPOS LIMA JUNIOR <jose.olino@trt20.intra>
2023-07-10 11:27:15 -0300

    primeiros exemplos

---
+++ type-number.js
@@ -0,0 +1,25 @@
+const number = 3;
+
+const num1 = 1;
+
+const num2 = 2;
+
+const operacao = num1 * num2;
+
+// Ponto Flutuante
+
+const numeroFloat = 3.3;
+
+const numeroSemZero = .5;
+
+// Codificação
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
